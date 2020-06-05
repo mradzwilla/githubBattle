a44b48c9f6cd0716db7668a7289d546114cc118e
mradzwilla <mradzwilla@gmail.com>
2020-06-05 01:58:31 -0400

    Updated copy plugin error

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
@@ -11,13 +12,18 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader']}
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from : '_redirects' }
+      ]
     })
   ],
   devServer: {
